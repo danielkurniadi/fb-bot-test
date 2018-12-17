c37eaff46cc77c0f3d1921ce0115dbb8c45c41fb
iqDF <daniel.thekurniadi@gmail.com>
2018-12-17 19:16:23 +0800

    "fix get response"

--- server/model/message/message.js
+++ server/model/message/message.js
@@ -17,16 +17,27 @@ const NORMAL_MESSAGE = 'NORMAL_MESSAGE';
  * @return {MessageResponse} the message response
  */
 
+const FAQ = require('../../../messages/faq');
+
+const getQuickReply = (quickReply)=>{
+	FAQ.forEach((message)=>{
+		if (message.payload == quickReply){
+			return message;
+		}
+	})
+};
 
 const getResponse = ({ text, payload }) => {
-
-	switch(payload){
-		
-	}
-
+	FAQ.forEach((message)=>{
+		if (message.payload == payload){
+			const quickReplies = message.quickReply.map(getQuickReply);
+			return quickReplies;
+		}
+	});
 };
 
 module.exports = {
 	getResponse,
+	getQuickReply,
 	CONSTANT: { QUICK_REPLY, NORMAL_MESSAGE }
 };
