08683c945576b61c759b35668812e6949877330e
iqDF <daniel.thekurniadi@gmail.com>
2018-12-22 18:22:14 +0800

    refactor unit test  for fb/message

--- testing/facebook.message.js
+++ testing/facebook.message.js
@@ -1,6 +1,7 @@
 const message = require('../server/model/facebook/message')
 const processMessage = message.processMessage;
 
+// event with NORMAL_MESSAGE
 let normalEvent = {
     "sender": {
         "id": "1978239305623940"
@@ -17,7 +18,8 @@ let normalEvent = {
     }
 }
 
-let quickRepEvent = {
+// event with QUICK_REPLY but no matching payload with those of our pool
+let falseQuickRepEvent = {
     "sender": {
         "id": "1254459154682919"
     },
@@ -35,13 +37,30 @@ let quickRepEvent = {
     }
 }
 
-//
-
+// event with QUICK_REPLY's payload matches with those of our pool
+let realQuickRepEvent ={
+    "sender": {
+        "id": "1254459154682919"
+    },
+    "recipient": {
+        "id": "682498171943165"
+    },
+    "timestamp": 1502905976377,
+    "message": {
+        "quick_reply": {
+        "payload": "GET_FAQ"
+        },
+        "mid": "m_AG5Hz2Uq7tuwNEhXfYYKj8mJEM_QPpz5jdCK48PnKAjSdjfipqxqMvK8ma6AC8fplwlqLP_5cgXIbu7I3rBN0P",
+        "seq": 9767,
+        "text": "Help me FAQ"
+    }
+}
 let responseNormal = processMessage(normalEvent);
-console.log(responseNormal);
+console.log("Normal Msg Event", responseNormal);
 
-let responseQuickRep = processMessage(quickRepEvent);
-console.log(quickRepEvent);
-        
+let responseQuickRep = processMessage(falseQuickRepEvent);
+console.log("Fake event:\n", responseQuickRep);
 
+responseQuickRep = processMessage(realQuickRepEvent);
+console.log("Real Quick Rep event:\n", responseQuickRep);
 
